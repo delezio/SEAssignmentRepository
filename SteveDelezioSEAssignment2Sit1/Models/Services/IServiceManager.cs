bed1c9a4a761917adf42c36eaea0bdd5190b03c6
delezio <steve.delezio@mcast.edu.mt>
2015-12-16 12:17:47 +0100

    Connected Service and folder creation  for design patterns

---
+++ SteveDelezioSEAssignment2Sit1/Models/Services/IServiceManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SteveDelezioSEAssignment2Sit1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceManager" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceManager
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
